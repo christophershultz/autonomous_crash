eb0e1c10e39f843b6e318ecaf07bcf97fa791a96
Christopher Shultz <c.shultz@live.com>
2025-08-30 08:22:31 -0400

    dropping high missing rate cols

--- s3_combine.py
+++ s3_combine.py
@@ -42,4 +42,17 @@ df['desc'] = [str(df['desc'][i]) + ' ' + str(df['addtl_desc'][i]) for i in range
 df = df.drop(columns = ['addtl_desc'])
 df['desc'] = [i[:-4] if i[-4:] == 'None' else i for i in df['desc']]
 
+# FINAL CLEANUP
+# DROPS DUE TO FILL RATE
+drops = ['avt number_2', 'license plate number', 'vehicle identification number',
+         'insurance company name or surety company at time of accident', 
+         'policy number', 'company naic number', 'from', 'to',
+         'license plate number_2', 'vehicle identification number_2', 
+         'drivers full name first middle last_2', 'driver license number_2', 
+         'date of birth_2', 'insurance company name or surety company at time of accident_2', 
+         'policy number_2', 'from_2', 'to_2', 'name first middle last', 'address', 'city',
+         'state_3', 'zip code', 'name first middle last_2', 'property damage', 'property owners name',
+         'witness name', 'telephone number_3', 'witness name_2']
+df = df.drop(columns = drops, axis = 1)
+
 df.to_csv('combined_dataset.csv', index = None, sep = "~")
